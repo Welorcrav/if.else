f8c5f1b4814e1ce46ae24ad599057bfee4125b7d
Welorcrav <igamberdiyevjahongir07@gmail.com>
2025-12-06 16:40:43 +0500

    Array folder added

---
+++ Array/script.js
@@ -0,0 +1,25 @@
+let fruits = ["apple", "banana", "ananas"];
+
+
+// let newFruits = fruits.slice(0,1);
+// console.log(newFruits);
+
+// console.log(fruits.indexOf("banana"));
+
+// fruits.unshift("grape");
+
+// fruits.shift()
+
+// fruits.pop("ananas");
+
+// fruits.push("peach", "cherry", 2, true)
+
+// fruits[2] = "peach";
+
+// console.log(fruits.includes("apple"));
+
+// fruits.forEach(fruit => console.log(fruit));
+
+console.log(fruits);
+
+
