f8aef1cb225bc618ac82cf24b96df177ef2e0572
Welorcrav <igamberdiyevjahongir07@gmail.com>
2025-12-06 16:13:57 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,185 @@
+//                                   Mission 1      EASY
+let son = 10
+
+if (son > 10) {
+  console.log('Katta')
+} else if (son < 10) {
+  console.log('Kichik')
+} else {
+  console.log('Teng')
+}
+
+//                                   Mission 2      EASY
+let yosh = 18
+
+if (yosh >= 18) {
+  console.log('Kattalar')
+} else {
+  console.log('Kichiklar')
+}
+
+//                                   Mission 3      EASY
+let raqam = 0
+
+if (raqam > 0) {
+  console.log('Musbat')
+} else if (raqam < 0) {
+  console.log('Manfiy')
+} else {
+  console.log('0')
+}
+
+//                                   Mission 4      EASY
+let son1 = 15
+let son2 = 10
+
+if (son2 > son1) {
+  console.log(son2)
+} else {
+  console.log(son1)
+}
+
+//                                   Mission 5      EASY
+let air = -1
+
+if (air > 0) {
+  console.log('Iliq')
+} else {
+  console.log('Sovuq')
+}
+
+//                                   Mission 6      EASY
+let ball = 70
+
+if (ball >= 50) {
+  console.log("O'tdi")
+} else {
+  console.log('Yiqildi')
+}
+
+//                                   Mission 7      EASY
+let password = '1234'
+
+if (password === '1234') {
+  console.log("To'g'ri")
+} else {
+  console.log('Xato')
+}
+
+//                                   Mission 8      EASY
+let login = ''
+
+if (login === 'welor') {
+  console.log('Qabul qilindi')
+} else if (login == '') {
+  console.log('Login kiriting!')
+} else {
+  console.log('Login xato!')
+}
+
+//                                   Mission 9      EASY
+let number = 3
+
+if (number % 2 === 0) {
+  console.log('Juft')
+} else {
+  console.log('Toq')
+}
+
+//                                   Mission 10      EASY
+let money = 100
+
+if (money > 0) {
+  console.log('Bor')
+} else {
+  console.log("Yo'q")
+}
+
+//                                   Mission 1      MEDIUM
+let gradus = 30
+
+if (gradus >= 30) {
+  console.log('Issiq')
+} else if (gradus >= 10) {
+  console.log('Normal')
+} else {
+  console.log('Sovuq')
+}
+
+//                                   Mission 1      MEDIUM
+let grade = 90
+
+if (grade >= 90) {
+  console.log('A')
+} else if (grade >= 80) {
+  console.log('B')
+} else if (grade >= 70) {
+  console.log('C')
+} else if (grade >= 60) {
+  console.log('D')
+} else {
+  console.log('F')
+}
+
+//                                   Mission 2      MEDIUM
+let num1 = 10,
+  num2 = 20,
+  num3 = 25
+
+if (num1 > num2) {
+  console.log(num1)
+} else if (num2 > num3) {
+  console.log(num2)
+} else {
+  console.log(num3)
+}
+
+//                                   Mission 3      MEDIUM
+let fizzBuzz = 15;
+
+if (fizzBuzz % 3 === 0 && fizzBuzz % 5 === 0) {
+  console.log('FizzBuzz')
+} else if (fizzBuzz % 3 === 0) {
+  console.log('Fizz')
+} else if (fizzBuzz % 5 === 0) {
+  console.log('Buzz')
+} else {
+  console.log("Bo'linmaydi");
+}
+
+//                                   Mission 5      MEDIUM
+let username = 'welor'
+let pass = '1234'
+
+if (username === 'welor' && pass === '1234') {
+  console.log('Welocome!')
+} else {
+  console.log('Error!')
+}
+
+//                                   Mission 6      MEDIUM
+let hour = 23;
+
+if (hour >= 5 && hour <= 11) {
+  console.log("Good Morning!");
+} else if (hour >= 12 && hour <= 17) {
+  console.log("Good Afternoon!");
+} else if (hour >= 18 && hour <= 22) {
+  console.log('Good Evening!')
+} else {
+  console.log("Good Night!");
+}
+
+
+
+//                                   Mission 7      MEDIUM
+let plus1 = 5;
+let plus2 = 10;
+
+const sum = plus1 + plus2;
+
+if (sum % 2 === 0) {
+  console.log("Even sum");
+} else {
+  console.log("Odd sum");
+}
